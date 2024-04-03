a8a1f09c0251ad5142c92f47bb25a1ecff433ee4
curious-99 <mohit39.ugcs20@iiitranchi.ac.in>
2024-04-04 00:00:45 +0530

    push

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,16 +4,32 @@ import { Button, TextField } from '@radix-ui/themes'
 import React from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import {
+  useForm,
+  Controller
+} from 'react-hook-form';
+
+interface Issueform{
+  title : string;
+  discription: string;
+}
 
 const NewIssuePage = () => {
+  const {register, control, handleSubmit} = useForm<Issueform>();
   return (
-    <div className='max-w-xl space-y-3'>
+    <form 
+    className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit((data) => console.log(data))}>
       <TextField.Root>
-        <TextField.Input placeholder='Title' />
+        <TextField.Input placeholder='Title' {...register('title')}/>
       </TextField.Root>
-      <SimpleMDE placeholder='Description' />
+      <Controller
+        name = 'description'
+        control={control}
+        register = {({field}) =>  <SimpleMDE placeholder='Description' {...field} /> }
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
